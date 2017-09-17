a9ae446ad5f7076720f11a8cac2a10f9301bf4e3
Towhid <towhid@stsbde.com>
2017-09-17 17:10:18 +0600

    Optimized to single activity

--- app/src/main/java/towhid/icurious/webview/MainActivity.java
+++ app/src/main/java/towhid/icurious/webview/MainActivity.java
@@ -1,17 +1,17 @@
 package towhid.icurious.webview;
 
-import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.webkit.WebSettings;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
 
@@ -19,33 +19,55 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.webView);
+        if (isOnline()) initWebView();
+        viewSplash();
+    }
 
-        mWebView = (WebView) findViewById(R.id.activity_main_webview);
-
-
-        // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
-
-        // Enable Javascript
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        // Use remote resource
-        mWebView.loadUrl(getString(R.string.web_url));
+    private void initWebView() {
+        mWebView.getSettings().setJavaScriptEnabled(true);
 
         // Stop local links and redirects from opening in browser instead of WebView
-        mWebView.setWebViewClient(new mAppWebViewClient());
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
 
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+            }
+        });
 
+        mWebView.loadUrl(getString(R.string.url));
+    }
+
+    private void viewSplash() {
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                getSupportActionBar().show();
+                findViewById(R.id.splash).setVisibility(View.GONE);
+            }
+        }, 3000);
     }
 
     @Override
     public void onBackPressed() {
-        if (mWebView.canGoBack()) {
-            mWebView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        if (mWebView.canGoBack()) mWebView.goBack();
+        else super.onBackPressed();
     }
 
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean connected = cm != null && cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
+
+        if (!connected) Toast.makeText(this,
+                "Internet Connection Required",
+                Toast.LENGTH_LONG).show();
+        return connected;
+    }
 }
