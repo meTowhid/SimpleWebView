7e4c8fd8ea4b011f5920596fd31727324f92512b
Towhid <tohidul.islam@shohoz.net>
2019-11-03 17:49:52 +0600

    feature -> VideoEnabledWebView added

--- app/src/main/java/towhid/icurious/webview/MainActivity.kt
+++ app/src/main/java/towhid/icurious/webview/MainActivity.kt
@@ -1,63 +1,62 @@
 package towhid.icurious.webview
 
-import android.content.Context
-import android.net.ConnectivityManager
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    private val chromeClient: VideoEnabledWebChromeClient by lazy {
+        object : VideoEnabledWebChromeClient(
+                nonVideoLayout, // Your own view, read class comments
+                videoLayout as ViewGroup, // Your own view, read class comments
+                layoutInflater.inflate(R.layout.view_loading_video, null), // Your own view, read class comments
+                webView) {
+            // Subscribe to standard events, such as onProgressChanged()...
+            override fun onProgressChanged(view: WebView, progress: Int) {
+                // Your code...
+            }
+        }.also {
+            it.setOnToggledFullscreen(object : VideoEnabledWebChromeClient.ToggledFullscreenCallback {
+                override fun toggledFullscreen(fullscreen: Boolean) {
+                    // Your code to handle the full-screen change, for example showing and hiding the title bar. Example:
+                    val attrs = window.attributes
+                    if (fullscreen) {
+                        attrs.flags = attrs.flags or WindowManager.LayoutParams.FLAG_FULLSCREEN
+                        attrs.flags = attrs.flags or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    } else {
+                        attrs.flags = attrs.flags and WindowManager.LayoutParams.FLAG_FULLSCREEN.inv()
+                        attrs.flags = attrs.flags and WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON.inv()
+                    }
+                    window.attributes = attrs
+                    if (android.os.Build.VERSION.SDK_INT >= 14)
+                        window.decorView.systemUiVisibility = if (fullscreen) View.SYSTEM_UI_FLAG_LOW_PROFILE else View.SYSTEM_UI_FLAG_VISIBLE
+                }
+            })
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        if (isOnline) initWebView()
-        viewSplash()
-    }
 
-    private fun initWebView() {
         webView?.apply {
-            settings.javaScriptEnabled = true
-
-            // Stop local links and redirects from opening in browser instead of WebView
+            setWebChromeClient(chromeClient)
             webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                    view.loadUrl(url)
-                    return true
-                }
-
-                override fun onPageFinished(view: WebView, url: String) {
-                    progressBar?.visibility = View.INVISIBLE
-                }
+                override fun shouldOverrideUrlLoading(view: WebView, url: String) = true.also { view.loadUrl(url) }
             }
-
             loadUrl(getString(R.string.url))
         }
-    }
-
-    private fun viewSplash() {
-        Handler().postDelayed({
-            splash?.visibility = View.GONE
-        }, 2000)
     }
 
     override fun onBackPressed() {
         if (webView != null && webView.canGoBack()) webView.goBack()
         else super.onBackPressed()
     }
-
-    private val isOnline: Boolean
-        get() {
-            val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            return (cm.activeNetworkInfo?.isConnectedOrConnecting == true).also {
-                if (!it) Toast.makeText(this, "Internet Connection Required", Toast.LENGTH_LONG).show()
-            }
-        }
 }
